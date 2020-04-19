42f8f1a4acc1139016354e7f36e81bd590dc8a3e
Aleksandr <mes-aleksandr@yandex.ru>
2020-04-19 17:03:46 +0500

    first commit

---
+++ img/index.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="yandex-verification" content="b4770c0f609c8385" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="диссертация заказ, магистерская работа, диплом заказ, работа заказ, заказ курсовой, купить курсовой, контрольная работа класс, контрольная работа по, лабораторная работа">
+    <meta name="description" content="Помощь студентам написание дипломны, курсовых, ВКР, рефератов, контролных, лабораторных работ по любым предметам. Консультации студентов по учебным вопросам от экспертов 8-800-333-85-44. Объяснение материала, пояснение по любой задаче. Любые предметы, любые сроки! Гарантия и договор. Удобная оплата. Оставляйте заявку!">
+    <link rel="stylesheet" href="style.css">
+    <?php wp_head(); ?>
+</head>
+
+<body>
+    <?php wp_body_open(); ?>
+    <div class="contaner">
+        <div class="header">
+            <div class="header-top">
+                <div class="header-logo"></div>
+                <div class="header-phone">
+                    <p>8-800-333-86-22<br> звонок бесплатный</p>
+                </div>
+                <!-- <div class="header-nav"></div> -->
+                <div class="header-consultant">
+                    <button class='consul' onclick="jivo_api.open();">ONLINE КОНСУЛЬТАНТ</button>
+                    <button class="order"><a href="#order" class="z-order-link">ЗАКАЗАТЬ РАБОТУ</a></button>
+                </div>
+            </div>
+            <div class="header-img">
+                <img src="<?php echo get_template_directory_uri() . '/img/kofe-karandasi-rabocij-stol-noutbuk-smartfon.jpg'; ?>" alt="header image">
+                <div class="header-img-bg">
+                    <div class="header-text">
+                        <p>
+                            <h1>Помощь студентам</h1><br>
+                            <span>Профессиональный подход - гарантированный результат</span>
+                            <span>Консультируем в написании:</span>
+                            <ul>
+                                <li>дипломных работ</li>
+                                <li>отчетов по практике</li>
+                                <li>контрольных работ</li>
+                                <li>курсовых работ</li>
+                                <li>рефератов</li>
+                                <li> чертежей</li>
+                                <li>задач</li>
+                                <li>других работ</li>
+                            </ul>
+                        </p>
+                    </div>
+                </div>
+
+                <div class="zao_order_vertical_short">
+                    <div id="zao_order_vertical_short"></div>
+                </div>
+
+
+
+            </div>
+
+        </div>
+        <div class="main">
+            <div class="otz">
+                <div id="zao_reviews_02" style="position:relative"></div>
+            </div>
+
+            <section class="section-why-as">
+                <h2 class="section-why-as__title">Почему мы?</h2>
+                <div class="row row-fix container-center">
+                    <article class="section-why-as__content-block">
+                        <ul class="small-block-grid-1 large-block-grid-3 grid-fix">
+                            <li class="section-why-as__card section-why-as__card-item-1">
+                                <div class="section-why-as-card__content-block">
+                                    <h2 class="card__title">Гарантия 100%</h2>
+                                    <div class="card__text-block">
+                                        <p class="card__text">
+                                            Мы выполняем качественные
+                                            <br>уникальные работы
+                                        </p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="section-why-as__card section-why-as__card-item-2">
+                                <div class="section-why-as-card__content-block">
+                                    <h2 class="card__title">По карману всем!</h2>
+                                    <div class="card__text-block">
+                                        <p class="card__text">
+                                            Стоимость наших студенческих
+                                            <br>работ от 60 руб. за лист
+                                        </p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="section-why-as__card section-why-as__card-item-3">
+                                <div class="section-why-as-card__content-block">
+                                    <h2 class="card__title">453051 Студентов</h2>
+                                    <div class="card__text-block">
+                                        <p class="card__text">
+                                            Воспользовались нашими
+                                            <br>услугами и остались довольны
+                                        </p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="section-why-as__card section-why-as__card-item-4">
+                                <div class="section-why-as-card__content-block">
+                                    <h2 class="card__title">Индивидуальный<br>подход</h2>
+                                    <div class="card__text-block">
+                                        <p class="card__text">
+                                            Индивидуальный подход к
+                                            <br>каждому клиенту.
+                                        </p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="section-why-as__card section-why-as__card-item-5">
+                                <div class="section-why-as-card__content-block">
+                                    <h2 class="card__title">1500 Опытных<br>авторов</h2>
+                                    <div class="card__text-block">
+                                        <p class="card__text">
+                                            Более 1500 опытных авторов
+                                            <br>– специалистов своей области.
+                                        </p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="section-why-as__card section-why-as__card-item-6">
+                                <div class="section-why-as-card__content-block">
+                                    <h2 class="card__title">Минимальные<br>сроки</h2>
+                                    <div class="card__text-block">
+                                        <p class="card__text">
+                                            Минимальные сроки написания
+                                            <br>работ и срочные заказы.
+                                        </p>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </article>
+                </div>
+            </section>
+
+            <div class="order">
+                <a name="order"></a>
+                <div id="zao_order_horizontal"></div>
+
+            </div>
+
+
+        </div>
+        <div class="footer">
+            <p>Все права защищены. 2020 &COPY;</p>
+        </div>
+    </div>
+    <script type="text/javascript">
+        if (!calculator_h_initiator) {
+            var calculator_h_initiator = window.document.createElement('script');
+            calculator_h_initiator.type = 'text/javascript';
+            calculator_h_initiator.async = true;
+            calculator_h_initiator.src = 'https://tracker.zaochnik.net/promo/forms/get_form?a_aid=e60cd12b&template_name=calculator_h&tag_name=calculator_h&chan=&data1=&data2=&promo_code=';
+            document.body.appendChild(calculator_h_initiator);
+        }
+    </script>
+    <script type="text/javascript"
+        src="https://zaochnik.com/get-a_aid-phone.php?a_aid_p=e60cd12b&a_bid=07d01236&phonechan=hsw_online&data1=phone"></script>
+
+    <script type='text/javascript'>
+        (function () {
+            var widget_id = 'lFlBWeI5zK';
+            var d = document;
+            var w = window;
+            function l() {
+                var s = document.createElement('script');
+                s.type = 'text/javascript';
+                s.async = true;
+                s.src = '//code.jivosite.com/script/widget/' + widget_id;
+                var ss = document.getElementsByTagName('script')[0];
+                ss.parentNode.insertBefore(s, ss);
+            }
+            if (d.readyState == 'complete') {
+                l();
+            } else {
+                if (w.attachEvent) {
+                    w.attachEvent('onload', l);
+                } else {
+                    w.addEventListener('load', l, false);
+                }
+            }
+        })();
+    </script>
+    <script type="text/javascript"
+        src="https://zaochnik.com/get-a_aid-phone.php?a_aid_p=e60cd12b&amp;a_bid=220d9e07&amp;data1=phone"></script>
+    <script type="text/javascript">
+        if (!zao_order_vertical_short_initiator) {
+            var zao_order_vertical_short_initiator = window.document.createElement("script");
+            zao_order_vertical_short_initiator.type = "text/javascript";
+            zao_order_vertical_short_initiator.async = true;
+            zao_order_vertical_short_initiator.src = "https://zchnk.ru/forms/zao-order-vertical-short.js?a_aid=e60cd12b&tag_name=zao_order_vertical_short&template_name=zao_order_vertical_short";
+            document.body.appendChild(zao_order_vertical_short_initiator);
+        }
+    </script>
+
+    <!-- заказатть работу -->
+    <script type="text/javascript">
+        if (!zao_order_horizontal_initiator) {
+            var zao_order_horizontal_initiator = window.document.createElement("script");
+            zao_order_horizontal_initiator.type = "text/javascript";
+            zao_order_horizontal_initiator.async = true;
+            zao_order_horizontal_initiator.src = "https://zchnk.ru/forms/zao-order-horizontal.js?a_aid=e60cd12b&tag_name=zao_order_horizontal&template_name=zao_order_horizontal&header_text=%D0%97%D0%B0%D0%BA%D0%B0%D0%B7%D0%B0%D1%82%D1%8C%20%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%83&submit_text=%D0%97%D0%90%D0%9A%D0%90%D0%97%D0%90%D0%A2%D0%AC&hide_additional=true";
+            document.body.appendChild(zao_order_horizontal_initiator);
+        }
+    </script>
+
+    <!-- отзывы -->
+    <script type="text/javascript">
+        if (!zao_reviews_02_initiator) {
+            var zao_reviews_02_initiator = document.createElement('iframe');
+            zao_reviews_02_initiator.src = 'https://r-money.ru/zao/promo/reviews/?tag_name=zao_reviews_02&a_aid=e60cd12b&mode=horizontal_wide&per_page=20';
+            zao_reviews_02_initiator.setAttribute("scrolling", "no");
+            zao_reviews_02_initiator.setAttribute("frameborder", "0");
+            zao_reviews_02_initiator.setAttribute("allowfullscreen", "true");
+            zao_reviews_02_initiator.style.width = "100%";
+            zao_reviews_02_initiator.style.height = "380px";
+            var iframe_container = document.getElementById('zao_reviews_02');
+            iframe_container.appendChild(zao_reviews_02_initiator);
+        }
+    </script>
+
+</body>
+
+</html>
